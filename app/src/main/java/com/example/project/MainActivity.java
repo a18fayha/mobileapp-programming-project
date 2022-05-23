bbff03ecd9899a55369b4c85d0c209377bf16541
a18fayha <62876354+a18fayha@users.noreply.github.com>
2022-05-23 07:03:39 +0200

    - Implemented onCreateOpttionMenu method to inflate the about menu - Also implemented OnOptionItemSelected method so that it starts a new activity when the menu option is clicked

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -54,5 +59,27 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         islandsList.addAll(tmp);
         adapter.notifyDataSetChanged();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_about, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.myItem) {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }

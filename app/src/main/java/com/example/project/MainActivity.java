332002d9ce6c380cb911e3c1984641819b7d2ab2
a18fayha <62876354+a18fayha@users.noreply.github.com>
2022-05-23 05:58:56 +0200

    - Initialized my recyclerview and set the adapter and layoutmanager - Implemented OnPostExecute method and populated the list wit json data

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -7,10 +7,20 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener{
+    private List<Islands> islandsList;
+    private RecyclerView recyclerView;
+    private Adapter adapter;
+
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a18fayha";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +28,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        islandsList = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        adapter = new Adapter(islandsList);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+
+
+
+
+
+
+
+        new JsonTask(MainActivity.this).execute(JSON_URL);
 
     }
 
+    @Override
+    public void onPostExecute(String json) {
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Islands>>(){}.getType();
+        List<Islands> tmp = gson.fromJson(json,type);
+        islandsList.addAll(tmp);
+        adapter.notifyDataSetChanged();
+
+    }
 }
